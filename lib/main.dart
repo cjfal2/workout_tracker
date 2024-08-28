1302d36c746f5f4e075cbc1ba46fc834bae42b41
kimdongjun <cjfal2@kakao.com>
2024-08-28 14:48:55 +0900

    0828 flutter 실습1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 void main() => runApp(
       DevicePreview(
@@ -18,8 +19,69 @@ class MyApp extends StatelessWidget {
       // useInheritedMediaQuery: true, // MediaQuery를 상속받아 사용
       locale: DevicePreview.locale(context), // Locale 설정
       builder: DevicePreview.appBuilder, // 앱 빌더 설정
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
+      // Theme config for FlexColorScheme version 7.3.x. Make sure you use
+// same or higher package version, but still same major version. If you
+// use a lower package version, some properties may not be supported.
+// In that case remove them after copying this theme to your app.
+      theme: FlexThemeData.light(
+        colors: const FlexSchemeColor(
+          primary: Color(0xff004881),
+          primaryContainer: Color(0xffd0e4ff),
+          secondary: Color(0xffac3306),
+          secondaryContainer: Color(0xffffdbcf),
+          tertiary: Color(0xff006875),
+          tertiaryContainer: Color(0xff95f0ff),
+          appBarColor: Color(0xffffdbcf),
+          error: Color(0xffb00020),
+        ),
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 7,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 10,
+          blendOnColors: false,
+          useTextTheme: true,
+          useM2StyleDividerInM3: true,
+          alignedDropdown: true,
+          useInputDecoratorThemeInDialogs: true,
+        ),
+        useMaterial3ErrorColors: true,
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        useMaterial3: true,
+        swapLegacyOnMaterial3: true,
+        // To use the Playground font, add GoogleFonts package and uncomment
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
+      ),
+      darkTheme: FlexThemeData.dark(
+        colors: const FlexSchemeColor(
+          primary: Color(0xff9fc9ff),
+          primaryContainer: Color(0xff00325b),
+          secondary: Color(0xffffb59d),
+          secondaryContainer: Color(0xff872100),
+          tertiary: Color(0xff86d2e1),
+          tertiaryContainer: Color(0xff004e59),
+          appBarColor: Color(0xff872100),
+          error: Color(0xffcf6679),
+        ),
+        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+        blendLevel: 13,
+        subThemesData: const FlexSubThemesData(
+          blendOnLevel: 20,
+          useTextTheme: true,
+          useM2StyleDividerInM3: true,
+          alignedDropdown: true,
+          useInputDecoratorThemeInDialogs: true,
+        ),
+        useMaterial3ErrorColors: true,
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        useMaterial3: true,
+        swapLegacyOnMaterial3: true,
+        // To use the Playground font, add GoogleFonts package and uncomment
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
+      ),
+// If you do not have a themeMode switch, uncomment this line
+// to let the device system mode control the theme mode:
+// themeMode: ThemeMode.system,
+
       home: const LandingPage(),
     );
   }
@@ -30,46 +92,53 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void printFunc() {
+      print(1);
+    }
+
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 50),
-        child: Center(
-          child: Column(
-            children: [
-              Flexible(
-                flex: 10,
-                child: Container(
-                  color: Color.fromRGBO(255, 136, 136, 1),
-                  child: Text(
+      backgroundColor: Colors.amber[200],
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/runner.png"),
+              opacity: 0.2,
+            ),
+          ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 50),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
                     "러닝앱",
-                    style: TextStyle(fontSize: 32),
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                        fontFamily:
+                            "NanumBrushScript"), // 테마 쓸 때 커스텀 폰트 패밀리 적용법
+                    // style: TextStyle(fontSize: 32, fontFamily: "NanumBrushScript"),
                   ),
-                ),
-              ),
-              Expanded(
-                flex: 100,
-                child: Image.asset("assets/runner.png"),
-              ),
-              Flexible(
-                flex: 10,
-                child: Text(
-                  "환영합니다",
-                  style: TextStyle(fontSize: 32),
-                ),
-              ),
-              Flexible(
-                flex: 10,
-                child: ElevatedButton(
-                  onPressed: () => print("1"),
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                  child: Text(
-                    "시작하기",
-                    style: TextStyle(fontSize: 24),
+                  Column(
+                    children: [
+                      Text(
+                        "환영합니다",
+                        style: TextStyle(fontSize: 32),
+                      ),
+                      ElevatedButton(
+                        onPressed: printFunc,
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green),
+                        child: Text(
+                          "시작하기",
+                          style: TextStyle(fontSize: 24),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
